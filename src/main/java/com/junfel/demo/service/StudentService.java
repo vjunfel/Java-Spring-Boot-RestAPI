71d51ac2aaafe4d2bc747893b1920922cfc6ac86
Junfel <vjunfel@gmail.com>
2026-02-23 23:19:36 +0800

    Add student service

--- src/main/java/com/junfel/demo/service/StudentService.java
+++ src/main/java/com/junfel/demo/service/StudentService.java
@@ -1,7 +1,23 @@
 package com.junfel.demo.service;
 
+import com.junfel.demo.entity.Student;
+import com.junfel.demo.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public void registerStudent(String name, String email) {
+        if (name == null || name.isBlank()) {
+            throw new RuntimeException("Invalid name");
+        }
+
+        studentRepository.save(new Student(name, email));
+    }
 }
